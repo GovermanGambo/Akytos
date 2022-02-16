768710b54acaa335e1f8e299e653944d679ea21d
Eirik Hemstad <ehe@dips.no>
2022-02-16 13:17:06 +0100

    Yes

--- src/Akytos/Serialization/SerializedObject.cs
+++ src/Akytos/Serialization/SerializedObject.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-using System.Reflection;
+using Akytos.Assertions;
 using YamlDotNet.Serialization;
 
 namespace Akytos;
@@ -16,7 +16,7 @@ public class SerializedObject
         Type = type;
     }
 
-    [YamlMember] public string Type { get; set; }
+    [YamlMember] public string Type { get; set; } = null!;
     [YamlMember] public SerializedField[] Fields { get; set; } = null!;
 
     public static SerializedObject Create(object o)
@@ -30,7 +30,7 @@ public class SerializedObject
             string type = fieldInfo.FieldType.FullName!;
             object? value = fieldInfo.GetValue(o);
 
-            if (!IsPrimitiveType(value.GetType()))
+            if (!IsPrimitiveType(fieldInfo.FieldType))
             {
                 if (value is IEnumerable enumerable)
                 {
@@ -53,7 +53,7 @@ public class SerializedObject
 
                     value = list;
                 }
-                else
+                else if (value != null)
                 {
                     // Create new serialized object if not value type
                     value = Create(value);
@@ -64,6 +64,60 @@ public class SerializedObject
         }
 
         return new SerializedObject(serializedFields.ToArray(), objectType.FullName!);
+    }
+
+    public static object Deserialize(SerializedObject serializedObject)
+    {
+        var objectType = System.Type.GetType(serializedObject.Type);
+        
+        Assert.IsNotNull(objectType, $"Type {serializedObject.Type} was not found!");
+        
+        object? instance = Activator.CreateInstance(objectType);
+        
+        Assert.IsNotNull(instance, $"Could not create instance of type {serializedObject.Type}!");
+
+        var fields = NodeUtils.GetSerializedFields(objectType).ToList();
+
+        foreach (var serializedField in serializedObject.Fields)
+        {
+            var fieldType = System.Type.GetType(serializedField.Type);
+            Assert.IsNotNull(fieldType, $"Type {serializedField.Type} was not found!");
+
+            var fieldInfo = fields.FirstOrDefault(f => f.Name == serializedField.Key);
+            object? value = DeserializeField(serializedField);
+            
+            Assert.IsNotNull(fieldInfo, $"Field with name {serializedField.Key} was not found!");
+            
+            fieldInfo.SetValue(instance, value);
+        }
+
+        return instance;
+    }
+
+    private static object? DeserializeField(SerializedField serializedField)
+    {
+        var type = System.Type.GetType(serializedField.Type);
+        
+        Assert.IsNotNull(type, $"Type {serializedField.Type} was not found!");
+        
+        if (IsPrimitiveType(type))
+        {
+            return serializedField.Value;
+        }
+        else if (serializedField.Value is IEnumerable enumerable)
+        {
+            var list = new List<object>();
+            foreach (object element in enumerable)
+            {
+            }
+        }
+
+        if (serializedField.Value is SerializedObject serializedObject)
+        {
+            return Deserialize(serializedObject);
+        }
+
+        throw new NotSupportedException();
     }
 
     private static bool IsPrimitiveType(Type type)
