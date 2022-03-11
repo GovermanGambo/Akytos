417caa041e4f1c945f04b77877aeaaded1e9cbe1
Eirik Hemstad <eirikzh@gmail.com>
2022-03-11 09:42:14 +0100

    Changed style

--- src/Akytos/Graphics/ImGui/ImGuiLayer.cs
+++ src/Akytos/Graphics/ImGui/ImGuiLayer.cs
@@ -79,11 +79,13 @@ internal class ImGuiLayer : ILayer
 
     private void SetDarkThemeColors()
     {
-        var colors = ImGui.GetStyle().Colors;
         ImGui.GetStyle().WindowRounding = 0.0f;
+        ImGui.GetStyle().TabRounding = 0.0f;
+        
+        var colors = ImGui.GetStyle().Colors;
         colors[(int) ImGuiCol.WindowBg] = new Vector4(0.1f, 0.105f, 0.11f, 1.0f);
             
-        colors[(int) ImGuiCol.Header] = new Vector4(0.2f, 0.205f, 0.21f, 1.0f);
+        colors[(int) ImGuiCol.Header] = new Vector4(0f, 0.4f, 0.2f, 1.0f);
         colors[(int) ImGuiCol.HeaderHovered] = new Vector4(0.3f, 0.305f, 0.31f, 1.0f);
         colors[(int) ImGuiCol.HeaderActive] = new Vector4(0.15f, 0.155f, 0.151f, 1.0f);
             
@@ -95,14 +97,14 @@ internal class ImGuiLayer : ILayer
         colors[(int) ImGuiCol.FrameBgHovered] = new Vector4(0.3f, 0.305f, 0.31f, 1.0f);
         colors[(int) ImGuiCol.FrameBgActive] = new Vector4(0.15f, 0.155f, 0.151f, 1.0f);
             
-        colors[(int) ImGuiCol.Tab] = new Vector4(0.15f, 0.155f, 0.151f, 1.0f);
-        colors[(int) ImGuiCol.TabHovered] = new Vector4(0.38f, 0.3805f, 0.381f, 1.0f);
-        colors[(int) ImGuiCol.TabActive] = new Vector4(0.28f, 0.2805f, 0.281f, 1.0f);
-        colors[(int) ImGuiCol.TabUnfocused] = new Vector4(0.15f, 0.155f, 0.151f, 1.0f);
-        colors[(int) ImGuiCol.TabUnfocusedActive] = new Vector4(0.2f, 0.205f, 0.21f, 1.0f);
+        colors[(int) ImGuiCol.Tab] = new Vector4(0f, 0.2f, 0f, 1.0f);
+        colors[(int) ImGuiCol.TabHovered] = new Vector4(0f, 0.8f, 0.3f, 1.0f);
+        colors[(int) ImGuiCol.TabActive] = new Vector4(0f, 0.6f, 0.3f, 1.0f);
+        colors[(int) ImGuiCol.TabUnfocused] = new Vector4(0f, 0.4f, 0.2f, 1.0f);
+        colors[(int) ImGuiCol.TabUnfocusedActive] = new Vector4(0f, 0.4f, 0.2f, 1.0f);
             
-        colors[(int) ImGuiCol.TitleBg] = new Vector4(0.15f, 0.155f, 0.151f, 1.0f);
-        colors[(int) ImGuiCol.TitleBgActive] = new Vector4(0.15f, 0.155f, 0.151f, 1.0f);
+        colors[(int) ImGuiCol.TitleBg] = new Vector4(0f, 0.2f, 0.1f, 1.0f);
+        colors[(int) ImGuiCol.TitleBgActive] = new Vector4(0f, 0.3f, 0.2f, 1.0f);
         colors[(int) ImGuiCol.TitleBgCollapsed] = new Vector4(0.15f, 0.155f, 0.151f, 1.0f);
         
         
